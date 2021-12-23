05e3bdb1932c8b025e2bbf0dba03da8b7373e0a1
Pulkitkhanna <63971237+pulkitkhanna1@users.noreply.github.com>
2021-12-23 17:08:38 +0530

    Add files via upload

---
+++ Webhook.py
@@ -0,0 +1,20 @@
+import flask
+import os
+from flask import send_from_directory
+
+app = flask.Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
